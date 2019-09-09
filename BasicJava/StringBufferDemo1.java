c6da69a2564c2f0168dab8755b10e2641244be5e
sivakalpana <sivakalpana1@gmail.com>
2019-09-09 14:00:06 +0530

    first changes in Basicjava program

---
+++ BasicJava/StringBufferDemo1.java
@@ -0,0 +1,18 @@
+package com.string;
+
+public class StringBufferDemo1 {
+ public static void main(String[] args) {
+	 StringBuffer s = new StringBuffer("Welcomes you");
+	StringBuffer newString =  s.append("!!!");
+	 System.out.println(newString);
+	 s.insert(0 , 'w');
+	 System.out.println(s);
+	 s.replace(0 , 2, "good");
+	 System.out.println(s);
+	 s.delete(0 ,  1);
+	 System.out.println(s);
+	 System.out.println(s.reverse());
+	 System.out.println(s.capacity());
+	 
+ }
+}
---
+++ BasicJava/StringBuilderDemo.java
@@ -0,0 +1,15 @@
+package com.string;
+
+public class StringBuilderDemo {
+	 public static void main(String[] args) {
+		 StringBuilder s1 = new StringBuilder("happy");
+		 s1.append("Learning");
+		 System.out.println(s1);
+		 System.out.println(s1.delete(0, 1));
+		 
+		 System.out.println(s1.insert(1, "Welcome"));
+		 System.out.println(s1.reverse());
+		 System.out.println(s1.capacity());
+		 
+	 }
+}
